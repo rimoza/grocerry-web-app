963a6f352ab523aadb6566832ab214ce588d83c2
RidwanMohmet <ridwan.maxamed1@gmail.com>
2022-06-03 20:35:52 +0300

    Add shop cart icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Fragment } from "react";
-
+import { ShoppingCartIcon } from "@heroicons/react/outline";
 const App = () => {
   return (
     <Fragment>
-      <header>
+      <header className="font-nunito">
         <div id="top_nav_bar">
           <div id="top_navbar_left" className="h-8 bg-secondary hidden md:flex">
             <div id="time_work_container">
@@ -23,9 +23,17 @@ const App = () => {
             <div id="logo">Shoparry</div>
           </div>
           <div id="right">
-            <div id="search_container">{/* search */}</div>
+            <div id="search_container">
+              <div id="input">
+                <input type="text" placeholder="Search" />
+              </div>
+            </div>
             <div id="user_container">{/* user */}</div>
-            <div id="cart_container">{/* cart */}</div>
+            <div id="cart_container">
+              <div id="cart_icon">
+                <ShoppingCartIcon width={20} height={20} />
+              </div>
+            </div>
           </div>
         </nav>
       </header>
