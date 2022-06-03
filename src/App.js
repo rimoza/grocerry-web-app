43c22958ee5f75921da67bf838aeea96533e0824
RidwanMohmet <ridwan.maxamed1@gmail.com>
2022-06-03 19:23:30 +0300

    Add header

--- src/App.js
+++ src/App.js
@@ -1,3 +1,30 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import { Fragment } from "react";
+
+const App = () => {
+  return (
+    <Fragment>
+      <header className="font-nunito">
+        <div id="top_nav_bar">
+          <div id="top_navbar_left">
+            <div id="time_work_container">{/* time work */}</div>
+            <div id="contact_container">{/* phone */}</div>
+          </div>
+          <div id="top_navbar_right">{/* languages */}</div>
+        </div>
+        <nav>
+          <div id="left">
+            <div id="logo">Shoparry</div>
+          </div>
+          <div id="right">
+            <div id="search_container">{/* search */}</div>
+            <div id="user_container">{/* user */}</div>
+            <div id="cart_container">{/* cart */}</div>
+          </div>
+        </nav>
+      </header>
+      <main></main>
+    </Fragment>
+  );
+};
+
+export default App;
