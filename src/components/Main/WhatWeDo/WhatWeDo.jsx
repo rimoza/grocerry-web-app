11e93582dc09c9e1a1f4085554a743bf2593453c
RidwanMohmet <ridwan.maxamed1@gmail.com>
2022-06-04 17:53:10 +0300

    Break down into smaller components

--- src/components/Main/WhatWeDo/WhatWeDo.jsx
+++ src/components/Main/WhatWeDo/WhatWeDo.jsx
@@ -1,35 +1,56 @@
 import React from "react";
 import classes from "./WhatWeDo.module.css";
 
-const WhatWeDo = () => {
+const Card = (props) => {
+  const { title, desc, img } = props;
   return (
-    <section className="flex justify-around items-center flex-wrap ga-5">
-      <div className={classes.container}>
-        <div className={classes.img_container}>
-          <img src="/assets/images/clock.png" alt="delivery time" />
-        </div>
-        <div className={classes.card_body}>
-          <h1>What we do</h1>
-          <p>We help thousands of people reduce food waste</p>
-        </div>
+    <div className={classes.container}>
+      <div className={classes.img_container}>
+        <img src={img} alt="delivery time" />
       </div>
-      <div className={classes.container}>
-        <div className={classes.img_container}>
-          <img src="/assets/images/clock.png" alt="delivery time" />
-        </div>
-        <div>
-          <h1>What we do</h1>
-          <p>We help thousands of people reduce food waste</p>
-        </div>
+      <div className={classes.card_body}>
+        <h1>{title}</h1>
+        <p>{desc}</p>
       </div>
-      <div className={classes.container}>
-        <div className={classes.img_container}>
-          <img src="/assets/images/clock.png" alt="delivery time" />
-        </div>
-        <div>
-          <h1>What we do</h1>
-          <p>We help thousands of people reduce food waste</p>
-        </div>
+    </div>
+  );
+};
+
+const WhatWeDo = () => {
+  const data = [
+    {
+      title: "Fast Delivery",
+      description: "Week 2 -3 hours after order",
+      img: "/assets/images/delivery.png",
+    },
+    {
+      title: "Opening Hours",
+      description: "Sat - Fri 6:00am - 9:00pm",
+      img: "/assets/images/clock.png",
+    },
+    {
+      title: "Eco- Friendly packaging",
+      description: "Sustainable materials",
+      img: "/assets/images/seedling.png",
+    },
+  ];
+  return (
+    <section className={classes.card_container}>
+      <div>
+        <h1 className={classes.card_title}>What We Do</h1>
+        <p className={classes.card_desc}>
+          We help thousands of people reduce food waste
+        </p>
+      </div>
+      <div className="flex justify-around items-center flex-wrap gap-5">
+        {data.map((item, index) => (
+          <Card
+            key={index}
+            title={item.title}
+            desc={item.description}
+            img={item.img}
+          />
+        ))}
       </div>
     </section>
   );
