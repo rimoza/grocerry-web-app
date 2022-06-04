c5e643eb1324a2600e6401a96003843192ea983b
RidwanMohmet <ridwan.maxamed1@gmail.com>
2022-06-04 17:35:16 +0300

    Add images whatWeDo component

--- src/components/Main/WhatWeDo/WhatWeDo.jsx
+++ src/components/Main/WhatWeDo/WhatWeDo.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 import classes from "./WhatWeDo.module.css";
+
 const WhatWeDo = () => {
-  return <div>WhatWeDo</div>;
+  return (
+    <section className="flex justify-around items-center flex-wrap ga-5">
+      <div className={classes.container}>
+        <div className={classes.img_container}>
+          <img src="/assets/images/clock.png" alt="delivery time" />
+        </div>
+        <div className={classes.card_body}>
+          <h1>What we do</h1>
+          <p>We help thousands of people reduce food waste</p>
+        </div>
+      </div>
+      <div className={classes.container}>
+        <div className={classes.img_container}>
+          <img src="/assets/images/clock.png" alt="delivery time" />
+        </div>
+        <div>
+          <h1>What we do</h1>
+          <p>We help thousands of people reduce food waste</p>
+        </div>
+      </div>
+      <div className={classes.container}>
+        <div className={classes.img_container}>
+          <img src="/assets/images/clock.png" alt="delivery time" />
+        </div>
+        <div>
+          <h1>What we do</h1>
+          <p>We help thousands of people reduce food waste</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default WhatWeDo;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Showcase from "./Showcase/Showcase";
+import WhatWeDo from "./WhatWeDo/WhatWeDo";
 
 const Main = () => {
   return (
     <main>
       <Showcase />
+      <WhatWeDo />
     </main>
   );
 };
