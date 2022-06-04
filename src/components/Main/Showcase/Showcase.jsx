cb2e407cf66a03c3eba76b0a825cf151ce60e24e
RidwanMohmet <ridwan.maxamed1@gmail.com>
2022-06-04 09:55:39 +0300

    Create showcase component

--- src/components/Main/Showcase/Showcase.jsx
+++ src/components/Main/Showcase/Showcase.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import classes from "./Showcase.module.css";
 const Showcase = () => {
-  return <div>Showcase</div>;
+  return (
+    <section className={classes.showcase_container}>
+      <div id="info_container">
+        <h1 className={classes.showcase_title}>
+          Freshly <span className="text-orange-500">picked</span> <br /> from
+          <span className="text-primary"> {""} Nature</span>
+        </h1>
+        <p className={classes.showcase_desc}>
+          We provide you with various fresh grocery with 10% <br /> off for
+          every $50 purchase
+        </p>
+      </div>
+      <div className={classes.showcase_btn}>
+        <button className={classes.showcase_btn_view}>View Sale</button>
+        <button className={classes.showcase_btn_normal}>Shop All</button>
+      </div>
+    </section>
+  );
 };
 
 export default Showcase;
